c73403a3903f012667b4ce46f2ced074ceab0df9
oen19 <oen19@ic.ac.uk>
2020-01-12 12:12:52 +0000

    added doc-strings for stack functions

--- stack.py
+++ stack.py
@@ -1,16 +1,38 @@
 class Stack:
+    """
+    Class implementing the abstract data type for collecting
+    elements.
+    """
 
     def __init__(self):
         self.stack = []
 
     def push(self, item):
+        """
+        Add an item to the stack.
+        :param item: item to be added to stack.
+        """
         self.stack.append(item)
 
     def pop(self):
+        """
+        Remove the most recently added item from the stack
+        and return it.
+        :return: most recently added item from the stack.
+        """
        return self.stack.pop()
 
     def peek(self):
+        """
+        Return the most recently added item from the stack
+        wothout removing it from the stack.
+        :return: most recently added item from the stack.
+        """
         return self.stack[-1]
 
     def is_empty(self):
+        """
+        Check if the stack is empty.
+        :return: if stack is empty:---> True, else: ---> False
+        """
        return len(self.stack) == 0
